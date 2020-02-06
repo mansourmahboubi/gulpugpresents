0bdae7db4045b7d478de9fa4d4cc9d9a3db234d9
M Mansour Mahboubi <mm.mahboubi@ut.ac.ir>
2020-02-06 09:44:44 +0330

    add django models

--- pug/models.py
+++ pug/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Persentation(models.Model):
+    title = models.CharField(max_length=50)
+    presenter = models.CharField(max_length=20)
+    descripton = models.CharField(max_length=250)
+    presentaion_link = models.URLField(max_length=300)
+    author_link = models.URLField(max_length=300)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created',)
+        verbose_name = 'Presentation'
+        verbose_name_plural = 'Presentations'
+
+    def __str__(self):
+        return self.title
